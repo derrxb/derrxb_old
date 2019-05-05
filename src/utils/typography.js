270195f22d489faabb72fb83b4b59d974e5b33cf
Derrick Bol <derrickbol@Derricks-MacBook-Pro.local>
2019-05-04 22:13:19 -0600

    Switch to default font
    Former-commit-id: a1954c086870faca8a8e952512aa4e243f084bda
    Former-commit-id: b1a575c44f0da3d6fb2515afe18314f6bace5974

--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,11 +1,24 @@
 import Typography from 'typography';
 import oceanBeachTheme from 'typography-theme-ocean-beach';
 
+const defaultFontFamily =
+  "-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif";
+
 oceanBeachTheme.overrideStyles = options => ({
   a: {
     boxShadow: 'none',
     textDecoration: 'underline',
+    fontFamily: 'inherit',
   },
+  body: {
+    fontFamily: defaultFontFamily,
+  },
+  h1: { fontFamily: 'inherit' },
+  h2: { fontFamily: 'inherit' },
+  h3: { fontFamily: 'inherit' },
+  h4: { fontFamily: 'inherit' },
+  h5: { fontFamily: 'inherit' },
+  h6: { fontFamily: 'inherit' },
 });
 
 const typography = new Typography(oceanBeachTheme);
