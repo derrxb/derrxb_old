a889924e8585ccb56df64315d568989f0de17e41
Derrick Bol <derrickbol@Derricks-MacBook-Pro.local>
2019-05-05 00:13:19 -0600

    Setup homepage to redirect to photography page
    Former-commit-id: a15b96566f966df0ff9942fa240df0f78147560e
    Former-commit-id: 129f5878124bd6d48ec0db384fc7b988587a4db6

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,13 +1,18 @@
 import React from 'react';
-
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { H1 } from '../components/shared';
 
 const NotFoundPage = () => (
   <Layout>
     <SEO title="404: Not found" />
-    <h1>NOT FOUND</h1>
-    <p>You just hit a route that doesn&#39;t exist... the sadness.</p>
+    <H1>
+      NOT FOUND...YET
+      <span role="img" aria-label="celebration emojis">
+        🎉🎉
+      </span>
+    </H1>
+    <p>This site is a work in progress. Do not worry, cool things are coming soon.</p>
   </Layout>
 );
 
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,41 +1,9 @@
-import React from 'react';
 import PropTypes from 'prop-types';
-import { Link } from 'gatsby';
-import Layout from '../components/layout';
-import SEO from '../components/seo';
-import { RecentBlogsList } from './photography';
 
-const IndexPage = ({ data }) => (
-  <Layout>
-    <SEO title="Home" keywords={[`belize photography`, `explore belize`]} />
-
-    <div
-      style={{
-        width: '100%',
-        height: 'auto',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-      }}
-    >
-      <h2>Hi! I build things and take pictures.</h2>
-
-      <h3>Recent</h3>
-
-      <RecentBlogsList>
-        <li>
-          <Link to="/mountain-pine-ridge-forest-reserve">
-            A day in the beautiful Mountain Pine Ridge
-          </Link>
-        </li>
-
-        <li>
-          <Link to="/cahal-pech-maya-ruins">Cahal Pech Maya Ruins</Link>
-        </li>
-      </RecentBlogsList>
-    </div>
-  </Layout>
-);
+const IndexPage = ({ data }) => {
+  window.location.assign('/photography');
+  return null;
+};
 
 IndexPage.defaultProps = {
   data: PropTypes.shape({
