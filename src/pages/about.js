16d30742e6670f46e248856c45af81d2ce596f3c
Derrick Bol <derrickbol33@gmail.com>
2019-06-28 23:48:48 -0600

    Add more information to About

--- src/pages/about.js
+++ src/pages/about.js
@@ -3,8 +3,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { graphql } from 'gatsby';
 import styled from 'styled-components';
+import Image from 'gatsby-image';
 import Layout from '../components/layout';
 import SEO from '../components/seo';
+import { Hero } from '../components/shared/Hero';
 
 const AboutWrapper = styled.div`
   a {
@@ -21,9 +23,20 @@ const AboutWrapper = styled.div`
 const About = ({ data }) => (
   <Layout>
     <SEO
-      title="about-me"
+      title="About Me"
       keywords={['belizean photographer', 'software developer in belize']}
     />
+
+    <Hero
+      style={{
+        height: 'auto',
+        maxHeight: '480px',
+        overflow: 'hidden',
+        paddingBottom: '1em',
+      }}
+    >
+      <Image fluid={data.about.frontmatter.heroImage.childImageSharp.fluid} />
+    </Hero>
 
     <AboutWrapper dangerouslySetInnerHTML={{ __html: data.about.html }} />
   </Layout>
@@ -39,6 +52,15 @@ export const aboutMeQuery = graphql`
   query {
     about: markdownRemark(frontmatter: { title: { eq: "about-me" } }) {
       html
+      frontmatter {
+        heroImage {
+          childImageSharp {
+            fluid(maxWidth: 2000, quality: 90) {
+              ...GatsbyImageSharpFluid
+            }
+          }
+        }
+      }
     }
   }
 `;
